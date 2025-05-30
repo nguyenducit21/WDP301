ae8bb1d3928390bfaa35e4228e827c45b7999de7
TuanLQHe171554 <tuanlqhe171554@fpt.edu.vn>
2025-05-30 10:18:05 +0700

    Explore Menu and FoodDisplay

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,12 +2,16 @@ import React, { useState } from "react";
 import "./Home.css";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
-
+import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
+import FoodDisplay from "../../components/FoodDisplay/FoodDisplay";
 
 const Home = () => {
+  const [category, setCategory] = useState("All");
   return (
     <div>
       <Header />
+      <ExploreMenu/>
+      <FoodDisplay category={category}/>
       <Footer/>
     </div>
   );
