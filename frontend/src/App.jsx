10b8834a73dba5997aa8b0aa45eb03a74371c313
nguyenducit21 <nguyenduc210203@gmail.com>
2025-05-30 09:42:26 +0700

    add navbar and header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,19 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import TableBookingPage from './pages/TableBookingPage';
+import React, { useState } from "react";
+import Navbar from "./components/Navbar/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
 
-function App() {
+const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/table-booking" element={<TableBookingPage />} />
-      </Routes>
-    </Router>
+    <>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
